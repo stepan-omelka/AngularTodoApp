6867c03884b52413e4de36afb0c50b829cd31d02
Štěpán Omelka <stepomelka@gmail.com>
2025-11-11 17:49:56 +0100

    feat: initial setup - add folders, todo-list component, todo-create component, modify router, delete default index app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {TodoList} from './components/todo-list/todo-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: TodoList
+  }
+];
